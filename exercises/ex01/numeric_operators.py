02cd3ade3970265534918720b2e5d4d0705db577
koreyb2 <koreyb@ad.unc.edu>
2021-08-26 14:02:40 -0400

    Finished Numeric Operators

---
+++ exercises/ex01/numeric_operators.py
@@ -0,0 +1,14 @@
+"""Numeric Operators."""
+
+__author__ = 730425339
+
+Left: int = int(input("Left-hand side: "))
+Right: int = int(input("Right-hand side: "))
+Line3 = int(Left) ** int(Right)
+print(str(Left) + " ** " + str(Right) + " is " + str(Line3))
+Line4 = int(Left) / int(Right)
+print(str(Left) + " / " + str(Right) + " is " + str(Line4))
+Line5 = int(Left) // int(Right)
+print(str(Left) + " // " + str(Right) + " is " + str(Line5))
+Line6 = int(Left) % int(Right)
+print(str(Left) + " % " + str(Right) + " is " + str(Line6))
