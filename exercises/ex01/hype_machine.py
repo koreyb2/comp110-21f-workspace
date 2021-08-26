3eabf1cf51a1bbc7559e32e6a303ec84968c21a6
koreyb2 <koreyb@ad.unc.edu>
2021-08-26 11:45:42 -0400

    Finished Hype Machine

--- exercises/ex01/hype_machine.py
+++ exercises/ex01/hype_machine.py
@@ -1,5 +1,12 @@
 # TODO: Write docstring here
-
+"""My Hype Machine!"""
 # TODO: Initialize __author__ variable here
-
+__author__ = 730425339
 # TODO: Implement your program logic here
+name: str = input("What is your name? ")
+print("You entered: ")
+print(name)
+
+print(str(name) + " is capable of doing anything!")
+print("You've got this " + str(name) + "!")
+print("Don't worry " + str(name) + ", you're going to kill it!")
