13b8c3a5072ecef745558e72172bb4abdd13e036
Korey Bauersfeld <koreyb@ad.unc.edu>
2021-09-07 11:33:38 -0400

    esxcape

--- sandbox/challenge_question1.py
+++ sandbox/challenge_question1.py
@@ -2,13 +2,13 @@
 
 choice: int = int(input("Enter a number: "))
 
-if choice > 50:
-    if choice > 75:
+if choice < 50:
+    if choice < 25:
         print("A")
     else:
         print("B")
 else:
-    if choice < 25:
+    if choice > 75:
         print("C")
     else:
         print("D")
