6d318605b90800baf335d95f84f33f28ddc3de15
Korey Bauersfeld <koreyb@ad.unc.edu>
2021-09-10 15:56:36 -0400

    Finished Tar Heels

--- exercises/ex03/tar_heels.py
+++ exercises/ex03/tar_heels.py
@@ -1,6 +1,21 @@
 """An exercise in remainders and boolean logic."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+__author__ = "730425339"
 
 
 # Begin your solution here...
+user_input: int = int(input("Enter an int: "))
+
+if user_input % 2 == 0:
+    if user_input % 7 == 0:
+        print("TAR HEELS")
+    else: 
+        print("TAR")
+
+if user_input % 7 == 0:
+    if user_input % 2 != 0:
+        print("HEELS")
+
+if user_input % 2 != 0:
+    if user_input % 7 != 0:
+        print("CAROLINA")
