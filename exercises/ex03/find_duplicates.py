21fe0dddf1a691626834ace019522354b477eacc
Korey Bauersfeld <koreyb@ad.unc.edu>
2021-09-14 13:36:51 -0400

    Done

--- exercises/ex03/find_duplicates.py
+++ exercises/ex03/find_duplicates.py
@@ -1,3 +1,18 @@
 """Finding duplicate letters in a word."""
 
-__author__ = "123456789"
+__author__ = "730425339"
+
+user_word: str = str(input("Enter a word: "))
+i: int = 0
+duplicate: str = "False"
+
+while i < len(user_word):
+    char = user_word[i]
+    j: int = i + 1
+    while j < len(user_word):
+        if char == user_word[j]:
+            duplicate = "True"
+        j += 1
+    i += 1
+duplicate: str = duplicate
+print("Found duplicate: " + duplicate)
