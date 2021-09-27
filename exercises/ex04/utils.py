fa0a1e15bb1af3aeb0cd82ae5412e19ae573a1c1
Korey Bauersfeld <koreyb@ad.unc.edu>
2021-09-26 21:52:48 -0400

    Finished part 1

--- exercises/ex04/utils.py
+++ exercises/ex04/utils.py
@@ -1,6 +1,30 @@
 """List utility functions."""
 
-__author__ = "123456789"
+__author__ = "730425339"
 
 
 # TODO: Implement your functions here.
+i: int = 0
+counter: int = 0
+
+
+def main() -> None:
+    """Main."""
+    print(all([1, 1, 1], 1))
+
+
+def all(x: list[int], y: int) -> bool:
+    """All."""
+    global counter
+    global i
+    while i < len(x):
+        if x[i] == y:
+            counter += 1
+        i += 1
+    if counter == len(x):
+        return True
+    return False
+
+
+if __name__ == "__main__":
+    main()
