f0b8958328686094c0e404ed99a97b5d567e07dc
Korey Bauersfeld <koreyb@ad.unc.edu>
2021-10-01 17:08:03 -0400

    Done

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -1,5 +1,46 @@
 """List utility functions part 2."""
 
-__author__ = "123456789"
+__author__ = "730425339"
 
 # Define your functions below
+
+
+def only_evens(x: list[int]) -> list[int]:
+    """Return only even numbers from a list."""
+    y: list[int] = []
+    i: int = 0
+    while i < len(x):
+        if x[i] % 2 == 0:
+            y.append(x[i])
+        i += 1
+    return y
+
+
+def sub(a: list[int], start: int, end: int) -> list[int]:
+    """Interval from a given list."""
+    subset: list[int] = []
+    if len(a) == 0 or start > len(a) or end <= 0:
+        return subset
+    if start < 0:
+        start = start - start
+    if end > len(a):
+        difference: int = end - len(a)
+        end = end - difference
+    while start < end:
+        subset.append(a[start])
+        start = start + 1
+    return subset
+
+
+def concat(first: list[int], second: list[int]) -> list[int]:
+    """Combining two lists."""
+    i: int = 0
+    combined: list[int] = []
+    while i < len(first):
+        combined.append(first[i])
+        i += 1
+    i = 0
+    while i < len(second):
+        combined.append(second[i])
+        i += 1
+    return combined
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -1,6 +1,51 @@
 """Unit tests for list utility functions."""
 
 # TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex05.utils import only_evens, sub, concat
+from exercises.ex05.utils import only_evens, sub, concat
 
-__author__ = "123456789"
+__author__ = "730425339"
+
+
+def test_only_evens_empty() -> None:
+    """Only evens function when the list is empty."""
+    assert only_evens([]) == []
+
+
+def test_only_evens_odds() -> None:
+    """Only evens function when the list is only odds."""
+    assert only_evens([1, 3, 5, 7, 9, 11]) == []
+
+
+def test_only_evens_mixed() -> None:
+    """Only evens function when the list is a mix of odds and evens."""
+    assert only_evens([1, 2, 3, 4, 5, 6, 7, 8, 9]) == [2, 4, 6, 8]
+
+
+def test_sub_start() -> None:
+    """Sub function when starting index is greater than the length."""
+    assert sub([1, 2, 3, 4], 5, 6) == []
+
+
+def test_sub_within() -> None:
+    """Sub function within a list."""
+    assert sub([4, 6, 8, 10, 12], 1, 3) == [6, 8]
+
+
+def test_sub_entire() -> None:
+    """Sub function over entire list except last value."""
+    assert sub([1, 2, 3, 4, 5, 6], 0, 5) == [1, 2, 3, 4, 5]
+
+
+def test_concat_empty() -> None:
+    """Concat with no arguments."""
+    assert concat([], []) == []
+
+
+def test_concat_one() -> None:
+    """Concat with only arguments in the second list."""
+    assert concat([], [3, 6, 9, 12, 15]) == [3, 6, 9, 12, 15]
+
+
+def test_concat_combine() -> None:
+    """Concat combining two equal length lists."""
+    assert concat([1, 2, 3, 4], [5, 6, 7, 8]) == [1, 2, 3, 4, 5, 6, 7, 8]
