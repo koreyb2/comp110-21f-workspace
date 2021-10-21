60d7d7124c33b30216387167f12179700d467a31
Korey Bauersfeld <koreyb@ad.unc.edu>
2021-10-20 22:47:49 -0400

    done

--- exercises/ex06/dictionaries.py
+++ exercises/ex06/dictionaries.py
@@ -1,5 +1,53 @@
 """Practice with dictionaries."""
 
-__author__ = "123456789"
+__author__ = "730425339"
 
 # Define your functions below
+
+
+def invert(x: dict[str, str]) -> dict[str, str]:
+    """Invert a given dictionary."""
+    y = dict([(b, a) for a, b in x.items()])
+    if len(y) < len(x):
+        raise KeyError("Duplicate keys found in inverted dictionary.")
+    return y
+
+
+print(invert({"e": "a", "y": "b", "z": "c"}))
+
+
+def favorite_color(x: dict[str, str]) -> str:
+    """Return the most mentioned color from a dictionary."""
+    favorite: str = ""
+    totals: dict[str, int] = {}
+    colors: list[str] = []
+    for a in x:
+        colors.append(x[a])
+    i: int = 0
+    while i < len(colors):
+        if colors[i] in totals:
+            totals[colors[i]] += 1
+        else:
+            totals[colors[i]] = 1
+        i += 1
+    print(totals)
+    return favorite
+
+
+print(favorite_color({"a": "green", "b": "blue", "c": "blue"}))
+
+
+def count(x: list[str]) -> dict[str, int]:
+    """Count the frequency of items in a list."""
+    a: dict[str, int] = {}
+    i: int = 0
+    while i < len(x):
+        if x[i] in a:
+            a[x[i]] += 1
+        else:
+            a[x[i]] = 1
+        i += 1
+    return a
+
+
+print(count(["a", "b", "c", "a"]))
--- exercises/ex06/dictionaries_test.py
+++ exercises/ex06/dictionaries_test.py
@@ -1,6 +1,53 @@
 """Unit tests for dictionary functions."""
 
 # TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex06.dictionaries import invert, favorite_color, count
+from exercises.ex06.dictionaries import invert, favorite_color, count
 
-__author__ = "123456789"
+__author__ = "730425339"
+
+
+def test_invert_one() -> None:
+    """Invert a dictionary with one item."""
+    assert invert({"a": "Carolina"}) == {"Carolina": "a"}
+
+
+def test_invert_full_dict() -> None:
+    """Invert a dictionary."""
+    assert invert({"a": "b", "c": "d", "e": "f"}) == {"b": "a", "d": "c", "f": "e"}
+
+
+def test_invert_dict_numbers() -> None:
+    """Dictionary with numbers as strings."""
+    assert invert({"a": "2", "b": "1", "c": "3"}) == {"2": "a", "1": "b", "3": "c"}
+
+
+def test_favorite_color_one() -> None:
+    """Return only color."""
+    assert favorite_color({"a": "blue"}) == "blue"
+
+
+def test_favorite_color_tie() -> None:
+    """A color is tied for the favorite."""
+    assert favorite_color({"a": "blue", "b": "blue", "c": "red", "d": "red"}) == "blue"
+
+
+def test_favorite_color_full() -> None:
+    """Return favorite color from full dict."""
+    assert favorite_color({"a": "blue", "b": "red", "c": "green", "d": "green"}) == "green"
+
+
+def test_count_empty() -> None:
+    """Count an empty list."""
+    assert count([]) == {}
+
+
+def test_count_one() -> None:
+    """List with one of each value."""
+    assert count(["a", "b", "c", "d"]) == {"a": 1, "b": 1, "c": 1, "d": 1}
+
+
+def test_count_multiple() -> None:
+    """Multiple of some values."""
+    assert count(["a", "b", "a", "b"]) == {"a": 2, "b": 2}
+
+
