b880aa1a780cc322c30ae24ba4957e5508850bf3
Korey Bauersfeld <koreyb@ad.unc.edu>
2021-10-28 12:01:42 -0400

    ok

--- exercises/ex07/data_utils.py
+++ exercises/ex07/data_utils.py
@@ -1,5 +1,54 @@
 """Utility functions."""
 
-__author__ = "123456789"
+__author__ = "730425339"
 
 # Define your functions below
+from csv import DictReader
+
+
+def read_csv_rows(filename: str) -> list[dict[str, str]]:
+    """Convert rows of CSV files into a list of dictionaries."""
+    table: list[dict[str, str]] = []
+    file_handle = open(filename, "r", encoding="utf8")
+    csv_reader = DictReader(file_handle)
+    for row in csv_reader:
+        table.append(row)
+    file_handle.close()
+    return table
+
+
+def column_values(table: list[dict[str, str]], column: str) -> list[str]:
+    """Produce a list[str] of all values in a single column."""
+    result: list[str] = []
+    for row in table:
+        item: str = row[column]
+        result.append(item)
+    return result
+
+
+def columnar(row_table: list[dict[str, str]]) -> dict[str, list[str]]:
+    """Transform a row-oriented table to a column-oriented table."""
+    result: dict[str, list[str]] = {}
+
+    first_row: dict[str, str] = row_table[0]
+    for column in first_row:
+        result[column] = column_values(row_table, column)
+
+    return result
+
+
+def head(x: dict[str, list[str]], N: int) -> dict[str, list[str]]:
+    """Table with only first N rows."""
+    result: dict[str, list[str]] = {}
+    for column in x:
+        n_values: list[str] = []
+        i: int = 0
+        while i <= N:
+            n_values.append(x[column][i])
+            i += 1
+        result[column] = n_values
+    return result
+
+
+def select(x: dict[str, list[str]], y: list[str]) -> dict[str, list[str]]:
+    """Table of specified columns."""
